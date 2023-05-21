7324520816b99ced358592a99d6f54904a61889e
Wismayasa <102472428+Wismayasa@users.noreply.github.com>
2023-05-21 20:05:07 +0800

    update

--- resources/views/app/app.blade.php
+++ resources/views/app/app.blade.php
@@ -5,6 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <link rel="icon" href="{{ asset('asset') }}/img/brand-img.png">
+
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
@@ -16,7 +18,7 @@
     
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
 
-    <title>Web Test</title>
+    <title>Tes</title>
     </head>
     <body>
         <div class="main-container d-flex">
